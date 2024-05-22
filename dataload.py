a45ad1a2c27cdad4878c9ec7635c01115249a2dc
tjahrfyd9472 <128373590+tjahrfyd9472@users.noreply.github.com>
2024-05-22 15:39:35 +0900

    Update dataload.py

--- dataload.py
+++ dataload.py
@@ -1,21 +1,22 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+           'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 # 데이터 불러오기
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
+   data = pd.read_csv(DATA_URL, nrows=nrows)
+   lowercase = lambda x: str(x).lower()
+   data.rename(lowercase, axis='columns', inplace=True)
+   data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+   return data
 
 # 텍스트 요소 생성. 사용자에게 데이터가 로드 되고 있음을 알린다.
 data_load_state = st.text('Loading data...')
@@ -29,3 +30,18 @@ data_load_state.text('Loading data...done!')
 # 부제목 만들기
 st.subheader('Raw data')
 st.write(data)
+
+# 데이터 시각화
+st.subheader('Pickup by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# 지도 시각화
+st.subheader('Pickup locations')
+pickup_map = st.map(data)
+
+# 상관관계 히트맵
+st.subheader('Feature correlation heatmap')
+st.write(sns.heatmap(data.corr(),annot=True))
+
+st.pyplot()
